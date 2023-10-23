ee9942510cd7529e275d5b6cd63c52a1a49a6f78
kevinnkansah <kevinnkansah6@gmail.com>
2023-10-23 14:45:28 +0200

    Made a KDE plot

--- Python/Testing/playground.py
+++ Python/Testing/playground.py
@@ -1,25 +1,13 @@
-def bodymassindex(height, weight):
-    return round((weight / height**2),2)
+a = 20
+b = 20 
 
-h = float(input("Enter your height in meters: "))
-w = float(input("Enter your weight in kg: "))
+print(a is b)
+print(a == b)
 
-print("Welcome to the BMI calculator.")
 
-bmi = bodymassindex(h, w)
 
-print("Your BMI is: ", bmi)
 
-if bmi <= 18.5:
-    print("You are underweight.")
-elif 18.5 < bmi <= 24.9:
-    print("Your weight is normal.")
-elif 25 < bmi <= 29.29:
-    print("You are overweight.")
-else:
-    print("You are obese.")
-    
-print("Press any key to exit.")
+
 
 
 
